35ac539542cad94c96125ff2753ab012c2a9412f
玖玖 <82791035+jiujiu-666@users.noreply.github.com>
2024-09-09 17:33:27 +0800

    Update common.js

--- common.js
+++ common.js
@@ -265,7 +265,7 @@ const _0x53fee8 = async (_0x3bc174, _0x2719db, _0x5af601 = 5) => {
 const _0x2c412b = async (_0x2b0217, _0x476507, _0x30fa7a, _0x3a8264 = 5) => {
 };
 const _0x171be3 = async (_0x3fe456, _0x5e4065, _0x5b7be9 = 5) => {
-  console["log"]("By 姜白白\n");
+  console["log"]("By jiujiu\n");
 };
 async function _0x5bcbae(_0x23aeb9) {
   return new Promise(_0x3b6a77 => {
